e3b1a296561d6b7d09573441816b3b7c0211a98d
yuorei <108039575+yuorei@users.noreply.github.com>
2024-06-03 18:01:40 +0900

    next/ogに変えた

--- src/app/entry/[id]/opengraph-image.tsx
+++ src/app/entry/[id]/opengraph-image.tsx
@@ -1,6 +1,7 @@
-import Image from "next/image";
+import { ImageResponse } from "next/og";
 import { getArticle } from "./getArticle";
 
+export const contentType = "image/png";
 export const revalidate = "force-cache";
 export const runtime = "nodejs";
 
@@ -10,40 +11,38 @@ export const size = {
   height: 630,
 };
 
-export default async function ImageComponent({ params }: { params: { id: string } }) {
+export default async function og({ params }: { params: { id: string } }) {
   const article = await getArticle(params.id);
-
-  return (
-    <div
-      style={{
-        fontSize: 48,
-        background: "white",
-        display: "flex",
-        flexDirection: "column",
-        alignItems: "center",
-        justifyContent: "center",
-        width: "100%",
-        height: "100%",
-      }}
-    >
-      <div
-        style={{ height: 40, backgroundColor: "#5AC8D8", width: "100%" }}
-      />
-      <h1
-        style={{
-          flex: 1,
-          maxWidth: "80%",
-          display: "flex",
-          alignItems: "center",
-          justifyContent: "center",
-        }}
-      >
-        {article.Title}
-      </h1>
-      <Image src={article.OGPURL} alt={article.Title} width={size.width} height={size.height} />
-      <div
-        style={{ height: 40, backgroundColor: "#5AC8D8", width: "100%" }}
-      />
-    </div>
-  );
+  try {
+    let imageResponse = new ImageResponse(
+      (
+        <div
+          style={{
+            width: "100%",
+            height: "100%",
+            display: "flex",
+            alignItems: "center",
+            justifyContent: "center",
+            fontSize: 88,
+            background: "#fff",
+            color: "#000",
+          }}
+        >
+          <img
+            src={
+              article.OGPURL ||
+              "https://blog.yuorei.com/opengraph-image.png"
+            }
+            alt={article?.Title}
+          />
+        </div>
+      ),
+      {
+        ...size,
+      }
+    );
+    return imageResponse;
+  } catch (error) {
+    console.error("OGP error: ", error);
+  }
 }
