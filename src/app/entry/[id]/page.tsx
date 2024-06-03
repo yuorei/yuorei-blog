de76c916a10af0b09dad097eb28de0279ccb50e9
yuorei <108039575+yuorei@users.noreply.github.com>
2024-06-03 18:24:01 +0900

    動的メタ

--- src/app/entry/[id]/page.tsx
+++ src/app/entry/[id]/page.tsx
@@ -1,45 +1,51 @@
-'use client'
-import ReactMarkdown from "react-markdown";
-import { notFound } from "next/navigation";
-import { useEffect, useState } from "react";
-import { Article } from '../../page';
-import styles from './article.module.css';
+import  ArticleComponent  from "@/app/components/Article";
+import { getArticle } from "./getArticle";
+import type { Metadata, ResolvingMetadata } from "next";
 
-
-export default function Article({ params }: { params: { id: string } }) {
-    const [article, setArticle] = useState<Article>();
-    useEffect(() => {
-        const fetchData = async () => {
-            try {
-                const response = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/blog/${params.id}`);
-                if (response.status === 404) {
-                    notFound();
-                }
-                if (!response.ok) {
-                    throw new Error(response.statusText);
-                }
-                const data = await response.json();
-                setArticle(data);
-            } catch (error) {
-                console.error('Error fetching data:', error);
-            }
-        };
-        fetchData();
-    }, [params.id]);
-    return (
-        <>
-            <div className="max-w-4xl mx-auto bg-white p-8 rounded-lg shadow-md">
-                <h1 className="text-4xl font-bold mb-4">{article?.Title}</h1>
-                <h2 className="text-gray-600 mb-4">{article?.Date}</h2>
-                <div className="relative w-full h-auto">
-                    <img
-                        src={article?.OGPURL as string}
-                        alt={article?.Title as string}
-                        className="w-full h-auto object-contain mb-4"
-                    />
-                </div>
-                <ReactMarkdown className={styles.md}>{article?.Content}</ReactMarkdown>
-            </div>
-        </>
-    );
+type Props = {
+  params: { id: string };
 };
+
+export async function generateMetadata(
+  { params }: Props,
+  parent: ResolvingMetadata
+): Promise<Metadata> {
+  const id = params.id;
+
+  let article = await getArticle(id);
+
+  return {
+    title: article?.Title || "yuorei blog",
+    description: article?.Content || "yuoreiのブログサイトです。",
+    openGraph: {
+      title: article?.Title || "yuorei blog",
+      description: article?.Content || "yuoreiのブログサイトです。",
+      type: "website",
+      url: `https://blog.yuorei.com/entry/${id}`,
+      images: [
+        article?.OGPURL ||
+          "https://blog.yuorei.com/opengraph-image.png",
+      ],
+    },
+    twitter: {
+      card: "summary_large_image",
+      title: article?.Title || "yuorei blog",
+      description: article?.Content || "yuoreiのブログサイトです。",
+      site: "@yuorei71",
+      creator: "@yuorei71",
+      images: [
+        article?.OGPURL ||
+          "https://blog.yuorei.com/opengraph-image.png",
+      ],
+    },
+  };
+}
+
+export default async function ArticlePage({
+  params,
+}: {
+  params: { id: string };
+}) {
+  const article = await getArticle(params.id);
+  return <ArticleComponent article={article} />;
+}
